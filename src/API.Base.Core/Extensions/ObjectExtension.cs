ead2ce1f693a6d88b3c65e4419683b9edec9fdc9
Burak Can Yükselen <burakcan.yukselen@gmail.com>
2021-08-31 10:50:51 +0300

    Update ObjectExtension.cs

--- src/API.Base.Core/Extensions/ObjectExtension.cs
+++ src/API.Base.Core/Extensions/ObjectExtension.cs
@@ -8,5 +8,20 @@ namespace API.Base.Core.Extensions
         {
             return new ApiResponse<object>(source) {Success = success};
         }
+        
+        public static TValue GetPropertyValue<TValue>(this PropertyInfo[] propertyInfos, object targetObject, params string[] propertyNames)
+        {
+            var propertyObject = propertyInfos.FirstOrDefault(p => propertyNames.Contains(p.Name, StringComparer.InvariantCultureIgnoreCase));
+            if (propertyObject == null)
+                return default;
+            if (typeof(TValue) == typeof(Guid))
+            {
+                var guid = new Guid(propertyObject?.GetValue(targetObject).ToString());
+                return (TValue) Convert.ChangeType(guid, typeof(TValue));
+            }
+
+            var objectValue = (TValue) propertyObject?.GetValue(targetObject);
+            return objectValue;
+        }
     }
 }
